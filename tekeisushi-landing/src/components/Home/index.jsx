7b8e0cd2f5dd993d61d8eeaae8e1de59e05ebecb
Luis Fernando Goytia Cabezas <luis.goytia.IT@gmail.com>
2023-05-21 21:12:46 -0300

    initial commit

---
+++ tekeisushi-landing/src/components/Home/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css';
+import WebGLFluid from "webgl-fluid"
+
+
+const Home = () => {
+    WebGLFluid(document.querySelector('canvas'), {
+        // options
+      })
+    return (
+        <div>
+            <h1>Home</h1>
+        </div>
+    )
+}
+export default Home
